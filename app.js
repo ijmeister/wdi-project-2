551247faaad28c1c3432cdbd90aea133a8584dc9
Raymond Aung <raymond.psaung@gmail.com>
2017-04-14 22:07:35 +0800

    additional setups and configs

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
+const path = require('path')
+const mongoose = require('mongoose')
+// const methodOverride = require('method-override')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+// const flash = require('connect-flash')
+require('dotenv').config()
 
-var app = express()
-var port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+var dbUrl
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.MONGO_DB_URI_TEST
+} else {
+  dbUrl = process.env.PROD_MONGODB || process.env.MONGO_DB_URI
+}
+mongoose.connect(dbUrl)
+mongoose.Promise = global.Promise
 
+const app = express()
+
+// configure session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({ url: dbUrl })
+}))
+
+// configure middlewares
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
   res.render('home')
 })
 
-app.listen(port, function () {
-  console.log('express server running at ' + port)
+app.listen(PORT, function () {
+  console.log('express server running at ' + PORT)
 })
